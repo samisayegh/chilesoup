3a51c6d906868cf5bb00c2909f43dd2c43696753
Sami Sayegh <samisayegh1@gmail.com>
2016-02-03 22:00:02 -0500

    updates to date and time pickers

--- chilesoup.js
+++ chilesoup.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	$('#datetimepicker').datetimepicker();
+	$('#datePicker').datetimepicker({
+		format:'MMM D (dddd)',
+	});
+
+	$('#fromTimePicker').datetimepicker({
+		format:'LT'
+	})
+
+	$('#untilTimePicker').datetimepicker({
+		format:'LT'
+	})
 });
